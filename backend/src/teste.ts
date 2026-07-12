07ef433c5054f1361a466868356b93e46db32364
DevVictorGutierres <devvictorgutierres@gmail.com>
2026-07-11 23:24:56 -0300

    removendo arquivo teste

--- backend/src/teste.ts
+++
@@ -1,8 +0,0 @@
-import bcrypt from "bcrypt";
-
-const senha = "Senha123";
-const hash = "$2b$10$prpfQbHNFpYpD7d/hatdm.5cCvQJTpO2MZHJsNU1dTTlBybIXvVmu";
-
-const resultado = await bcrypt.compare(senha, hash);
-
-console.log(resultado);
