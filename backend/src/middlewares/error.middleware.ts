23350ebf6b60683fe0bfe95abc4ed7a979969d19
DevVictorGutierres <devvictorgutierres@gmail.com>
2026-07-17 23:55:03 -0300

    feat: includes middleware error

---
+++ backend/src/middlewares/error.middleware.ts
@@ -0,0 +1,25 @@
+import type { Request, Response, NextFunction } from "express";
+import  { AppError } from "../utils/AppError.js";
+
+export const errorHandlerMiddleware = ( err: unknown, req: Request, res: Response, next: NextFunction) => {
+    console.error(err);
+
+    if (err instanceof AppError) {
+        return res.status(err.statusCode).json({
+            status: "error",
+            message: err.message,
+        });
+    }
+
+    if (err instanceof Error) {
+        return res.status(500).json({
+            status: "error",
+            message: err.message,
+        });
+    }
+
+    return res.status(500).json({
+        status: "error",
+        message: "Erro interno do servidor.",
+    });
+};
